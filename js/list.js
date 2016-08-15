988082ba13c52e680b44bc2ecdd07fb88078e283
yidongwuxian <yidongwuxian@sina.com>
2016-08-15 18:05:04 +0800

    帮衣坊

--- js/list.js
+++ js/list.js
@@ -11,4 +11,33 @@ $(function(){
 		});
 	});
 	//结算数量 end
+
+	//开启modal
+	$('#selectService').click(function(){
+		$('#bangModal').modal({
+		  keyboard: true
+	   })
+	});
+	//选择服务内容和价格
+	$('#bangSave').click(function(){
+		$('input[name="bangcheckbox"]:checked').each(function(){
+			 $('#selectService').html("点击修改");
+			 $('.serviceCn').html($(this).prev().text()).show();
+			 $('.servicePrice').css('display','inline-block');
+			 $('.serviceMinPrice').html($(this).parent().parent().find('.serviceMinPriceVal').text());
+			 $('.serviceMaxPrice').html($(this).parent().parent().find('.serviceMaxPriceVal').text());
+			 $('#bangModal').modal('hide');
+			 $(this).prop("checked", false );
+		});
+	});
+
+	//限制textarea最多输入50个字 start
+	var commitTextArea = $('#commitTextArea');
+	commitTextArea.keyup(function(){
+		var commitLen = $(this).val().length;
+		if(commitLen > 49){
+			$(this).val($(this).val().substring(0,50));
+		}
+	});
+	//限制textarea最多输入50个字 end
 });
