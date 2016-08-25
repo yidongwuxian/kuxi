492eea767e6141cd909db0ddc863e0699a09600a
yidongwuxian <yidongwuxian@sina.com>
2016-08-25 15:19:12 +0800

    意见反馈ctroller

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -2,17 +2,36 @@ app.controller('feedbackCtrl', function($scope, DataGetterService,$localStorage,
 	DataGetterService.getData(function(data){$scope.items= data;}, Constants.feedback_url);
 });
 
+app.config(function($httpProvider){
+	$httpProvider.defaults.headers.post['Content-Type']='application/x-www-form-urlencoded;charset=utf-8';
+});
 
-app.controller('writefeedbackCtrl', function($scope,$http, DataGetterService,$localStorage,Constants) {
+app.controller('writefeedbackCtrl', function($scope,$http,$localStorage) {
 	$scope.submitFeedback = function(){
-		$http.jsonp(httpx+'/api/commit_feedback.do?&callback=JSON_CALLBACK&&CONTENT='+$scope.feedback.textarea+ '&ACCOUNT_ID='＋'80C0E58E443B46A09CE1CE14B904D490' + '&TOKEN='＋'d0ce6306-f0f9-4740-900f-355f45675d93' + '&REQ_TYPE=01').success(
-		　　function(data){
-				if(data.result){
-				}else{
-					$localStorage.reset();
-					alert(data.resp_msg)
-				}
-		　　}
-		);
+		$scope.ajax=function(){
+	         $scope.postData='CONTENT='+$scope.feedback.textarea +'&ACCOUNT_ID='+$localStorage.ACCOUNT_ID+'&TOKEN='+$localStorage.TOKEN+'&REQ_TYPE=01';   //注意拼接形式
+			 $http({
+		         url:'http://111.198.143.96:11211/api/commit_feedback.do?&callback=JSON_CALLBACK&&',
+		         method:'post',
+		         data: $scope.postData
+		     })
+	         .success(function(data){
+	             $scope.items=data.result;
+	         })
+	         .error(function(err){
+	             console.log(err);
+	         })
+		 }
 	}
 });
+
+
+// $http.jsonp(httpx+'/api/commit_feedback.do?&callback=JSON_CALLBACK&&CONTENT='++ '&ACCOUNT_ID='＋'80C0E58E443B46A09CE1CE14B904D490' + '&TOKEN='＋'d0ce6306-f0f9-4740-900f-355f45675d93' + '&REQ_TYPE=01').success(
+// 　　function(data){
+// 		if(data.result){
+// 		}else{
+// 			$localStorage.reset();
+// 			alert(data.resp_msg)
+// 		}
+// 　　}
+// );
