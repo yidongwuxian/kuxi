d53c75f42f070a850e57ceba7973a85ca95d5f68
wolfKirin <353399484@qq.com>
2016-08-29 09:31:08 +0800

    列表页通过二级分类，筛选上衣和裤子的数据

--- controllers/clothes.js
+++ controllers/clothes.js
@@ -1,30 +1,8 @@
-//var app = angular.module('myApp', []);
-// app.controller('clothTabCtrl', ['$scope','$state', function ($scope, $state) {
-// 	$scope.myTab = [{
-// 			title: '上装外套',
-// 			url: 'Jacket.tpl'
-// 		}, {
-// 			title: '下装裤裙',
-// 			url: 'Culotte.tpl'
-// 		}];
-//
-// 	$scope.currentTab = 'Jacket.tpl';
-//
-// 	$scope.onClickTab = function (tab) {
-// 		$scope.currentTab = tab.url;
-// 	}
-//
-// 	$scope.isActiveTab = function(tabUrl) {
-// 		return tabUrl == $scope.currentTab;
-// 	}
-//
-// 	$scope.goserviceInfo = function ($state) {
-// 		$state.go('pages/serviceInfo.html');
-// 	}
-// }]);
 app.controller('clothTabCtrl', clothTabCtrl);
 function clothTabCtrl($scope, DataGetterService, Constants,$state) {
-	DataGetterService.getData(function(data){$scope.lists= data.goodsList;}, Constants.list_url);
+	DataGetterService.getData(function(data){
+	   $scope.lists = data.goodsList;
+	}, Constants.list_url);
 	$scope.goserviceInfo = function(){
 		$state.go("serviceInfo", {}, { reload: true });
 	}
