c2e31d05ef7647cbe3cc163b05f422c1e0c3e360
walkersing <walker_v5@sina.com>
2016-09-04 11:05:25 +0800

    商品列表，订单提交

--- controllers/clothes.js
+++ controllers/clothes.js
@@ -1,40 +1,74 @@
 
 
 app.controller('clothTabCtrl', clothTabCtrl);
-function clothTabCtrl($scope, DataGetterService, Constants, $state) {
+function clothTabCtrl($scope, DataGetterService,$localStorage, Constants, $state) {
 
-	$scope.num =0;
-	$scope.boughtList = boughtList;
-	$scope.total = 0;
-	for(var k in boughtList){
-	   if(boughtList[k]){
-		   $scope.total += boughtList[k].num*boughtList[k].price;
-	   }
-    }
+	$scope.totalPrice=0;
+	$scope.totalNum=0;
+	$scope.categoryList = [];
+	$scope.goodsList = {};
 
-	var common = {
-		 getTotal : function(total){
-		     total = 0;
-		     for(var k in boughtList){
-		         if(boughtList[k]){
-		             if(boughtList[k].num <=0){
-		                 boughtList[k].num = 0;
-		             }
-		             total += boughtList[k].num*boughtList[k].price;
-		         }
-		     }
-		     return total;
-		 }
+	 $scope.selectItem = function($event,price,goodsId, categoryId) {
+        var ch = $event.target;
+        if(! ch.checked){
+        	ch.checked=true;
+        }else {
+        	ch.checked = null;
+        }
+        if (ch.checked){
+        	$scope.totalNum++;
+        	$scope.totalPrice =accAdd($scope.totalPrice,price);
+        	for(var i=0,goods;goods = $scope.result[categoryId][i++];){
+        		if (goods.GOODS_ID == goodsId) {
+        			goods.GOODS_NUM=1;
+        			$scope.goodsList[goodsId] = goods;
+        			break;		
+        		}
+        	}
+        	
+        } else {
+        	$scope.totalNum--;
+        	$scope.totalPrice = Subtr($scope.totalPrice, price);
+        	delete $scope.goodsList[goodsId];
+
+        }
 	}
 
-	 $scope.buyAction = function(event){
-		 var id=event.target.getAttribute("item-id");
-		 var price=event.target.getAttribute("price");
-		 $scope.num+=1;
-     }
+	$scope.submitForm = function(){
+    	if ($scope.totalNum<1) {
+    		alert('请选择后提交');
+    		return false;
+    	};
+    	DataGetterService.isLogin();
+		$localStorage.OBJECT = $scope.goodsList;
+		$state.go('orderSubmit',{},{reload:true})
+    	
+    }
+    //加法   
+	function accAdd(arg1,arg2){   
+		var r1,r2,m;   
+		try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}   
+		try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0}   
+		m=Math.pow(10,Math.max(r1,r2))   
+		return (arg1*m+arg2*m)/m   
+	}   
+	//减法   
+	function Subtr(arg1,arg2){  
+	    var r1,r2,m,n;  
+	    try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}  
+	    try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0}  
+	    m=Math.pow(10,Math.max(r1,r2));  
+	    n=(r1>=r2)?r1:r2;  
+	    return ((arg1*m-arg2*m)/m).toFixed(n);  
+	}
 
 	DataGetterService.getData(function(data){
-	   $scope.items = data.goodsList;
+		$scope.result = data;
+	  	$scope.categoryList = $scope.result.categoryList;
+	  	for (var i = 0,ca; ca=$scope.categoryList[i++]; ) {
+	  		var key= ca.GOODS_CATEGORY_ID;
+	  		$scope.result[key].selectedStyle=false;
+	  	};
 	}, Constants.list_url);
 
 	$scope.hobby = false;
